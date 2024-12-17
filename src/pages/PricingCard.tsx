2d1e4d82d530da76006de26747f6a09dd674df0b
looju <omofade2020@gmail.com>
2024-12-17 14:29:28 +0100

    complete pricing card

--- src/pages/PricingCard.tsx
+++ src/pages/PricingCard.tsx
@@ -5,7 +5,7 @@ import {
   useMotionValue,
   useSpring,
 } from "framer-motion";
-import { FiMousePointer } from "react-icons/fi";
+import { IoIosStar } from "react-icons/io";
 
 const ROTATION_RANGE = 32.5;
 const HALF_ROTATION_RANGE = 32.5 / 2;
@@ -56,29 +56,53 @@ export const PricingCard = ({ type }: typeProps) => {
         transformStyle: "preserve-3d",
         transform,
       }}
-      className="relative h-96 w-72 rounded-xl bg-gradient-to-br from-indigo-300 to-violet-300"
+      className="relative h-96 w-72 mt-10 rounded-xl bg-gradient-to-br from-indigo-300 to-violet-300"
     >
       <div
         style={{
           transform: "translateZ(75px)",
           transformStyle: "preserve-3d",
         }}
-        className="absolute grid h-96 w-72 place-content-center rounded-xl bg-white shadow-lg"
+        className="absolute grid h-96 w-72  rounded-xl bg-[#00593F] shadow-lg"
       >
-        <FiMousePointer
-          style={{
-            transform: "translateZ(75px)",
-          }}
-          className="mx-auto text-4xl"
-        />
-        <p
-          style={{
-            transform: "translateZ(50px)",
-          }}
-          className="text-center text-2xl font-bold"
-        >
-          HOVER ME
-        </p>
+        <div className="px-5">
+          <p className=" text-center text-2xl text-white font-semibold mt-2">
+            {type == "member" ? "Member" : "Owner"}
+          </p>
+          <p className=" text-center text-base text-white font-normal mt-5">
+            {type == "member"
+              ? "For space allocations and bookings, we charge a fee of"
+              : "For every space booked out, we charge a fee of"}
+          </p>
+          <p className="text-center text-white mt-16 font-bold text-4xl">
+            {type == "member" ? "0%" : "2.17%"}
+          </p>
+          <p className="text-center text-white  font-extralight text-sm">
+            Exluding VAT
+          </p>
+          <div className="flex flex-row items-center justify-center">
+            <p className="text-center text-white  font-extralight text-sm mt-5">
+              {type == "member" ? (
+                <div className="flex flex-row items-center justify-center">
+                  <IoIosStar color="gold" className=" mr-2" />
+                  <p>All bookings are free</p>
+                </div>
+              ) : (
+                <div className="flex flex-row items-center justify-center">
+                  <IoIosStar color="gold" className=" mr-2" />
+                  <p>No upfront membership required</p>
+                </div>
+              )}
+            </p>
+          </div>
+
+          <div className="flex flex-row items-center justify-center">
+            <IoIosStar color="gold" className="mt-5 mr-2" />
+            <p className="text-center text-white  font-extralight items-center text-sm mt-5">
+              No monthly payments required
+            </p>
+          </div>
+        </div>
       </div>
     </motion.div>
   );
